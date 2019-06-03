0a899b2be0fb71433527683e59e09d7cc447f8f7
Thom <spamtrap-w00t+github+31459@unixium.net>
2019-06-03 01:41:58 -0500

    green(ish) bar check-in whoops! implemented UserController before tests.  Time to comment out code. User Controller stubbed out.

--- src/main/java/org/dieterich/WSECUTechChallenge/Controllers/UserController.java
+++ src/main/java/org/dieterich/WSECUTechChallenge/Controllers/UserController.java
@@ -1,17 +1,52 @@
 package org.dieterich.WSECUTechChallenge.Controllers;
 
+import org.dieterich.WSECUTechChallenge.DataAccess.UserService;
+import org.dieterich.WSECUTechChallenge.Exceptions.NothingFoundException;
 import org.dieterich.WSECUTechChallenge.Models.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    protected class UserError {
+        private String statusString;
+        private String errorMessage;
+
+        protected UserError() {}
+
+        public UserError setStatusString(String statusString) {
+            this.statusString = statusString;
+            return this;
+        }
+
+        public String getStatusString() {
+            return statusString;
+        }
+
+        public UserError setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+            return this;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
 
     @GetMapping("/{username}")
-    public User getUserByUsername(@PathVariable String username) {
-        return new User();
+    public User getUserByUsername(@PathVariable String username) throws NothingFoundException {
+        return userService.getUserByUsername(username);
+    }
+
+    @ExceptionHandler(NothingFoundException.class)
+    public UserError handleNotFoundException(NothingFoundException nfe) {
+        UserError result = new UserError();
+        result.setErrorMessage("Cannot find any matching user");
+        result.setStatusString("Not Found");
+        return result;
     }
 }
--- src/test/groovy/org/dieterich/WSECUTechChallenge/UserControllerUnitTest.groovy
+++ src/test/groovy/org/dieterich/WSECUTechChallenge/UserControllerUnitTest.groovy
@@ -1,10 +1,24 @@
 package org.dieterich.WSECUTechChallenge
 
 import org.dieterich.WSECUTechChallenge.Controllers.UserController
+import org.dieterich.WSECUTechChallenge.DataAccess.UserService
+import org.dieterich.WSECUTechChallenge.DataStorage.MemoryStorage
 import spock.lang.Specification
 
 class UserControllerUnitTest extends Specification {
+    UserService mockUserService
+    UserController subject
+
     def setup() {
-        def subject = new UserController()
+        subject = new UserController()
+        subject.metaClass.static.setService = { newObj ->
+            userService = newObj
+        }
+        mockUserService = Mock(UserService)
+        subject.setStore(mockUserService)
+    }
+
+    def "should return an existing user"() {
+
     }
 }
